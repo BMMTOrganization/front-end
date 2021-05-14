5f2af4700ce84cdf7b2e7a373d38f63f3802a6d7
jorge <jorgetapia0293@gmail.com>
2021-05-13 21:12:50 -0400

    successful migration

--- src/app/models/contact.ts
+++ src/app/models/contact.ts
@@ -1,8 +1,47 @@
 export class Contact {
-  public id: number;
-  public number: string;
-  public email: string;
-  public department: string;
+  private id: number;
+  private number: string;
+  private email: string;
+  private department: string;
+
+  // tslint:disable-next-line:variable-name
+  constructor(id: number, number: string, email: string, department: string) {
+    this.id = id;
+    this.number = number;
+    this.email = email;
+    this.department = department;
+  }
 
 
+  public getId(): number {
+    return this.id;
+  }
+
+  public setId(value: number) {
+    this.id = value;
+  }
+
+  public getNumber(): string {
+    return this.number;
+  }
+
+  public setNumber(value: string) {
+    this.number = value;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public setEmail(value: string) {
+    this.email = value;
+  }
+
+  public getDepartment(): string {
+    return this.department;
+  }
+
+  public setDepartment(value: string) {
+    this.department = value;
+  }
 }
