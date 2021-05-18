b726c05ee9b2d04a3ba0516b23d618f8da68cff9
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-05-18 14:25:14 -0400

    lots of stuff not initiated

--- src/app/models/contact.ts
+++ src/app/models/contact.ts
@@ -4,10 +4,10 @@ export class Contact {
   public email: string;
   public department: string;
 
-  // tslint:disable-next-line:variable-name
-  constructor(id: number, number: string, email: string, department: string) {
+
+  constructor(id: number, phoneNumber: string, email: string, department: string) {
     this.id = id;
-    this.number = number;
+    this.number = phoneNumber;
     this.email = email;
     this.department = department;
   }
@@ -17,7 +17,7 @@ export class Contact {
     return this.id;
   }
 
-  public setId(value: number) {
+  public setId(value: number): void {
     this.id = value;
   }
 
@@ -25,7 +25,7 @@ export class Contact {
     return this.number;
   }
 
-  public setNumber(value: string) {
+  public setNumber(value: string): void {
     this.number = value;
   }
 
@@ -33,7 +33,7 @@ export class Contact {
     return this.email;
   }
 
-  public setEmail(value: string) {
+  public setEmail(value: string): void {
     this.email = value;
   }
 
@@ -41,7 +41,7 @@ export class Contact {
     return this.department;
   }
 
-  public setDepartment(value: string) {
+  public setDepartment(value: string): void {
     this.department = value;
   }
 }
--- src/app/models/transaction.ts
+++ src/app/models/transaction.ts
@@ -1,9 +1,20 @@
 export class Transaction{
-  id: number;
-  transactionDate: Date;
-  accountOne: number;
-  accountTwo: number;
-  transactionType: string;
-  amount: number;
-  userId: number;
+  public id: number;
+  public transactionDate: Date;
+  public accountOne: number;
+  public accountTwo: number;
+  public transactionType: string;
+  public amount: number;
+  public userId: number;
+
+  constructor(id: number, transactionDate: Date, accountOne: number, accountTwo: number, transactionType: string,
+              amount: number, userId: number) {
+    this.id = id;
+    this.transactionDate = transactionDate;
+    this.accountOne = accountOne;
+    this.accountTwo = accountTwo;
+    this.transactionType = transactionType;
+    this.amount = amount;
+    this.userId = userId;
+  }
 }
